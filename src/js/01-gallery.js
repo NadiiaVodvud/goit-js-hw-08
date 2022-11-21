26dc7e7705b26842103349494f89b91c772d64ef
NadiiaVodvud <108605727+NadiiaVodvud@users.noreply.github.com>
2022-11-21 22:27:16 +0200

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,48 +1,23 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-// const SimpleLightbox = require('simpleLightbox');
-
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
 console.log(galleryItems);
 
 const container = document.querySelector('.gallery');
+
 const picturesGallery = galleryItems
   .map(
     image =>
-      `<div class="gallery__item"><a class="gallery__link" href=${image.original}><img class="gallery__image" src=${image.preview} data-source=${image.original} alt=${image.description}/></a></div>`
+      `<a class="gallery__item" href=${image.original}><img class="gallery__image" src=${image.preview} alt=${image.description}/></a>`
   )
   .join('');
 
 container.insertAdjacentHTML('afterbegin', picturesGallery);
 
-function onClick(evt) {
-  evt.preventDefault();
-
-  if (evt.target.nodeName !== 'IMG') {
-    return;
-  }
-
-  const largeImageUrl = evt.target.dataset.source;
-
-  const instance = basicLightbox.create(`<img src="${largeImageUrl}">`, {
-    onShow: instance => {
-      document.addEventListener('keydown', onEscapeKeyPress);
-    },
-    onClose: instance => {
-      document.removeEventListener('keydown', onEscapeKeyPress);
-    },
-  });
-
-  instance.show();
-
-  function onEscapeKeyPress(evt) {
-    if (evt.code === 'Escape') {
-      instance.close();
-    }
-  }
-}
-
-container.addEventListener('click', onClick);
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
