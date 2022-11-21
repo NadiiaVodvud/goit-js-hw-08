9329e8d8f96d9000839f20b85a1cb55a6c390e83
NadiiaVodvud <108605727+NadiiaVodvud@users.noreply.github.com>
2022-11-21 17:48:13 +0200

    task on 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,6 +3,9 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// const SimpleLightbox = require('simpleLightbox');
+
 console.log(galleryItems);
 
 const container = document.querySelector('.gallery');
