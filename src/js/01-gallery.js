1d6aebf7ce4cb059b79f33e2205f2e2026f6ddb8
NadiiaVodvud <108605727+NadiiaVodvud@users.noreply.github.com>
2022-11-21 16:33:16 +0200

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,45 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 console.log(galleryItems);
+
+const container = document.querySelector('.gallery');
+const picturesGallery = galleryItems
+  .map(
+    image =>
+      `<div class="gallery__item"><a class="gallery__link" href=${image.original}><img class="gallery__image" src=${image.preview} data-source=${image.original} alt=${image.description}/></a></div>`
+  )
+  .join('');
+
+container.insertAdjacentHTML('afterbegin', picturesGallery);
+
+function onClick(evt) {
+  evt.preventDefault();
+
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const largeImageUrl = evt.target.dataset.source;
+
+  const instance = basicLightbox.create(`<img src="${largeImageUrl}">`, {
+    onShow: instance => {
+      document.addEventListener('keydown', onEscapeKeyPress);
+    },
+    onClose: instance => {
+      document.removeEventListener('keydown', onEscapeKeyPress);
+    },
+  });
+
+  instance.show();
+
+  function onEscapeKeyPress(evt) {
+    if (evt.code === 'Escape') {
+      instance.close();
+    }
+  }
+}
+
+container.addEventListener('click', onClick);
