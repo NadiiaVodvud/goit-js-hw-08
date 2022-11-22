8445e5337502da1a4ba559b6a9ffb25ad991de9a
NadiiaVodvud <108605727+NadiiaVodvud@users.noreply.github.com>
2022-11-22 15:41:08 +0200

    start 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,52 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe);
+
+const onPlay = function (data) {
+  console.log(videoplayer - current - time);
+};
+// 'videoplayer-current-time';
+// const onPlay = function (data) {
+//   // data is an object containing properties specific to that event
+// };
+
+player.on('play', onPlay);
+
+// player.on('eventName', function (data) {
+//   // data is an object containing properties specific to that event
+// });
+
+// const player = new Player('handstick', {
+
+// player.on('play', function () {
+//   console.log('played the video!');
+// });
+
+// player.getVideoTitle().then(function (title) {
+//   console.log('title:', title);
+// });
+
+// player.getCurrentTime().then(function(seconds) {
+//     // seconds = the current playback position
+// }).catch(function(error) {
+//     // an error occurred
+// });
+
+// player
+//   .setCurrentTime(30.456)
+//   .then(function (seconds) {
+//     // seconds = the actual time that the player seeked to
+//   })
+//   .catch(function (error) {
+//     switch (error.name) {
+//       case 'RangeError':
+//         // the time was less than 0 or greater than the video’s duration
+//         break;
+
+//       default:
+//         // some other error occurred
+//         break;
+//     }
+//   });
