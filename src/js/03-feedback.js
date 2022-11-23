c97bd481f8a2ba728cb9129d96d7b6d6b2d73eb1
NadiiaVodvud <108605727+NadiiaVodvud@users.noreply.github.com>
2022-11-23 23:44:51 +0200

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {
+  email: form.email.value,
+  message: form.message.value,
+};
+
+fillFormTextarea();
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormInput(e) {
+  formData[e.target.name] = e.target.value.trim();
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+
+  console.log(formData);
+}
+
+function fillFormTextarea() {
+  const saveMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (saveMessage) {
+    form.value = saveMessage;
+  }
+}
