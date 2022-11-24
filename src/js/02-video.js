5e21d11100b25f912a9153a9b7970354e5d749a0
NadiiaVodvud <108605727+NadiiaVodvud@users.noreply.github.com>
2022-11-24 22:48:22 +0200

    done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,52 +2,15 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-
 const player = new Player(iframe);
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
 
-const onPlay = function (data) {
-  console.log(videoplayer - current - time);
-};
-// 'videoplayer-current-time';
-// const onPlay = function (data) {
-//   // data is an object containing properties specific to that event
-// };
+player.on('timeupdate', throttle(stopPlayTime, 1000));
 
-player.on('play', onPlay);
+function stopPlayTime() {
+  player.getCurrentTime().then(function (seconds) {
+    localStorage.setItem(LOCALSTORAGE_KEY, seconds);
+  });
+}
 
-// player.on('eventName', function (data) {
-//   // data is an object containing properties specific to that event
-// });
-
-// const player = new Player('handstick', {
-
-// player.on('play', function () {
-//   console.log('played the video!');
-// });
-
-// player.getVideoTitle().then(function (title) {
-//   console.log('title:', title);
-// });
-
-// player.getCurrentTime().then(function(seconds) {
-//     // seconds = the current playback position
-// }).catch(function(error) {
-//     // an error occurred
-// });
-
-// player
-//   .setCurrentTime(30.456)
-//   .then(function (seconds) {
-//     // seconds = the actual time that the player seeked to
-//   })
-//   .catch(function (error) {
-//     switch (error.name) {
-//       case 'RangeError':
-//         // the time was less than 0 or greater than the video’s duration
-//         break;
-
-//       default:
-//         // some other error occurred
-//         break;
-//     }
-//   });
+player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY) || 0);
